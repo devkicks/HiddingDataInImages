0efde259bc90c46d59328e7132f83fda7c977145
masad801 <masad.801@gmail.com>
2016-09-20 01:37:15 +0100

    Update Tweak

--- HiddenImageData/main.cpp
+++ HiddenImageData/main.cpp
@@ -46,7 +46,7 @@ void encodeImage(cv::Mat &inImage, char *textToEncode, int fileSize)
 			if(bitCount < 32)
 			{
 				// encode the fileSize
-				ch1 = ch1 | (((fileSize >> bitCount) & 1) << bitCount);
+				ch1 = ch1 & ((fileSize >> bitCount) & 1);
 				curPoint[0] = ch1;
 			}
 			else
@@ -55,8 +55,9 @@ void encodeImage(cv::Mat &inImage, char *textToEncode, int fileSize)
 				int curBit = curPos%8;
 				int curChar = curPos/8;
 
-				ch1 = ch1 | ((textToEncode[curChar] >> curBit & 1) << curBit);
+				ch1 = ch1 & (textToEncode[curChar] >> curBit & 1);
 			}
+			bitCount++;
 		}
 	}
 }
@@ -75,7 +76,7 @@ void decodeImage(cv::Mat &inImage, char *&textEncoded, int &fileSize)
 			if(bitCount < 32)
 			{
 				// encode the fileSize
-				fileSize = fileSize | (((ch1 >> bitCount) & 1) << ch1);
+				fileSize = fileSize | ((ch1 & 1) << bitCount);
 				//curPoint[0] = ch1;
 			}
 			/*else
@@ -86,6 +87,8 @@ void decodeImage(cv::Mat &inImage, char *&textEncoded, int &fileSize)
 
 				ch1 = ch1 | ((textToEncode[curChar] >> curBit | 0) << curBit);
 			}*/
+
+			bitCount++;
 		}
 	}
 
@@ -93,6 +96,15 @@ void decodeImage(cv::Mat &inImage, char *&textEncoded, int &fileSize)
 
 int main()
 {
+	int testing = 0;
+	int testing2 = 121;
+
+	for(int i = 0; i < 32; i++)
+	{
+		testing = testing | ((testing2 >> i) & 1) << i;
+	}
+	std::cout << testing <<std::endl;
+
 	int fileSize;
 	char *textToEncode;
 
@@ -102,7 +114,7 @@ int main()
 	inImage = cv::imread("TheMartian.jpg");
 
 	cv::imshow("Show Image", inImage);
-	cv::waitKey(0);
+	//cv::waitKey(0);
 
 	// encode the data in the image
 	
@@ -112,5 +124,5 @@ int main()
 	decodeImage(inImage, textEncoded, encodedFileSize);
 
 	std::cout << "Encoded FileSize was: " << encodedFileSize <<std::endl;
-	return 0;
+ 	return 0;
 }
